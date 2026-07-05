fa7bdcc5577e5d815fe1e5a22cd2171fbb526c72
João Inacio <inaciofilho.silva@gmail.com>
2026-07-04 21:28:07 -0300

    Atualiza item_name que recebia uma lista vazia em um dicionario contido na variavel store

--- inventory.py
+++ inventory.py
@@ -1,7 +1,7 @@
 """Sistema de gerenciamento de Inventario"""
 
 # Contem as variaveis do sistema
-item_list = []
+store= {}
 option= 0
 
 # Contem o menu
@@ -10,30 +10,37 @@ print("1- Adicionar")
 print("2- Remover")
 print("3- Atualizar")
 print("4- Listar")
-print("5- Sair")
-print("================")
+print("5- Lista Detalhada")
+print("6- Sair")
+print("=" * 50)
 
 # Contem a logica do menu
-while option != 5 :
+while option != 6 :
     option = int(input("Escolha uma opcao:  "))
     if option == 1:
         print("Voce escolheu a opcao 1.")
         item_name = input("Qual item deseja adicionar? \n")
-        item_list.append(item_name)
+        unit = int(input("Quantas unidades ? \n"))
+        description = input("Descreva o item. \n")
+        value = float(input("Valor por unidade ? \n"))
+        store[item_name] = {
+            "valor" : value, 
+            "unidades" : unit,
+            "descricao" : description
+        }
     elif option == 2 :
         print("Voce escolheu a opcao 2.")
         item_name = input("Qual item deseja remover ? \n")
-        item_list.remove(item_name)
-        print(item_list)
+        store.remove(item_name)
+        print(store)
     elif option == 3 :
-        question = input(f"Voce ecolheu opcao 3, qual iten deseja atualizar? \n {item_list} \n")
-        item_index = item_list.index(question)
+        question = input(f"Voce ecolheu opcao 3, qual iten deseja atualizar? \n {store} \n")
+        item_index = store.index(question)
         new_item = input("Qual o nome do novo item? \n")
-        item_list[item_index] = new_item
-        print(f"Item atualizado com sucesso!\n {item_list}")
+        store[item_index] = new_item
+        print(f"Item atualizado com sucesso!\n {store}")
     elif option == 4 :
         print("Voce escolheu a opcao 4.")
-        print(item_list)
+        print(store)
     else :
         print("Saindo do sistema.")
-
